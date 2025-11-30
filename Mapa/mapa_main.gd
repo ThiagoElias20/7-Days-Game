2a7aba0cbe79a60f1a2962468380436a29bf2be8
Peagah <108700195+OPeaga@users.noreply.github.com>
2025-11-29 23:40:17 -0300

    Lógica de entrar em casa

--- Mapa/mapa_main.gd
+++ Mapa/mapa_main.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
+var cena_pai = get_parent()
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 		# IMPORTANTE: Verifique se o nó do seu personagem na cena se chama exatamente "Player"
-	if body.name == "Player":		
+	if body.name == "Player" and GlobalVar.is_day_completed == true:
+		# Troca para a cena do mapa principal
+		get_tree().change_scene_to_file("res://EncerramentoDias/EncerramentoDia.tscn")
+		
+	if body.name == "Player" and GlobalVar.is_day_completed == false:
 		# Troca para a cena do mapa principal
 		get_tree().change_scene_to_file("res://Mapa/Player_House.tscn")
+		
+		
