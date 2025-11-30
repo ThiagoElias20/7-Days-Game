26ca89f69cebfaf74dffc9d6d8ed2dce574f2cdd
Peagah <108700195+OPeaga@users.noreply.github.com>
2025-11-29 23:40:54 -0300

    Lógica de sair da casa com base no dia

--- Assets/Scripts/porta-casa.gd
+++ Assets/Scripts/porta-casa.gd
@@ -4,7 +4,9 @@ func _on_body_entered(body: Node2D) -> void:
 	# Verifica se quem encostou na porta foi o Player
 	# IMPORTANTE: Verifique se o nó do seu personagem na cena se chama exatamente "Player"
 	if body.name == "Player":
-		print("Saindo de casa...")
+		print("Saindo de casa..." + str(GlobalVar.dia))
 		
 		# Troca para a cena do mapa principal
-		get_tree().change_scene_to_file("res://mapa.tscn")
+		get_tree().change_scene_to_file("res://Gameplay/Dia" + str(GlobalVar.dia) + ".tscn")
+		
+		
