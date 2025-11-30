582c0bfc35c4394f4c7162edd225a4cd72f1fbc2
Peagah <108700195+OPeaga@users.noreply.github.com>
2025-11-30 16:15:43 -0300

    Add files via upload

--- Gameplay/dia_1.gd
+++ Gameplay/dia_1.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
-
+@onready var bg_song = $BackGroundSong 
 
 func _ready():
 	GlobalVar.dia = 1
 	GlobalVar.total_time = 0
 	GlobalVar.score = 0
+	GlobalVar.is_day_completed = false
+	GlobalVar.is_secondary_task_completed = false
+	bg_song.play()
+
+func _on_time_counter_timeout() -> void:
+	GlobalVar.time_elapsed_day += 1
