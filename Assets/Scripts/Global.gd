526a88f97a3a067f896a84d93d92786130c9faf3
Peagah <108700195+OPeaga@users.noreply.github.com>
2025-11-29 23:39:36 -0300

    Atualizado

--- Assets/Scripts/Global.gd
+++ Assets/Scripts/Global.gd
@@ -1,6 +1,12 @@
 extends Node
 
-var dia : int
-var score : float
-var risk : float
-var total_time : float
+var dia : int = 0
+var score : float = 0.0
+var risk : float = 0.0
+var total_time : float = 0.0
+var time_elapsed_day : float = 0.0
+var is_day_completed : bool = false
+var is_secondary_task_completed : bool = false
+
+var PontuacoesPrimarias = [300,400,500,600,700]
+var PontuacoesSecundarias = [150, 200, 250, 300, 350]
