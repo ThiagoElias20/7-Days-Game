6a919159bc7d95ef5ed6ec83b85027e50d7ddab3
PH <ph.ph365@gmail.com>
2025-12-01 23:32:20 -0300

    mudanca na mapa-main

--- Mapa/mapa_main.gd
+++ Mapa/mapa_main.gd
@@ -9,8 +9,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		# Troca para a cena do mapa principal
 		get_tree().change_scene_to_file("res://EncerramentoDias/EncerramentoDia.tscn")
 		
-	if body.name == "Player" and GlobalVar.is_day_completed == false:
-		# Troca para a cena do mapa principal
-		get_tree().change_scene_to_file("res://Mapa/Player_House.tscn")
+	#if body.name == "Player" and GlobalVar.is_day_completed == false:
+		## Troca para a cena do mapa principal
+		#get_tree().change_scene_to_file("res://Mapa/Player_House.tscn")
 		
 		
