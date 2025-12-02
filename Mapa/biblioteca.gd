f55cb2cf7d80a450a297a0a0cb1bf66ba727ccda
PH <ph.ph365@gmail.com>
2025-12-01 21:19:09 -0300

    biblioteca parcialmente finalizada

--- Mapa/biblioteca.gd
+++ Mapa/biblioteca.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
-@onready var armarioFalso = $"Chões/AmárioFalso"
+@onready var armarioFalso = $"Chões/ArmárioFalso"
+@onready var colisorArmario = $"Descobre Armario Falso/Colisor Armário"
+
 
 func _on_descobre_armario_falso_body_entered(body: Node2D) -> void:
-	armarioFalso.hide()
+	if body.name == "Player":
+		armarioFalso.hide()
+		colisorArmario.disabled = true
+
+
+func _on_entra_na_portinha_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		null
